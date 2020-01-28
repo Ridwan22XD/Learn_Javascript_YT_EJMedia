34ce7a80f0acda108c8317bdaf605b58a567307d
Ridwan <mre22xd@gmail.com>
2020-01-28 11:58:10 +0700

    using parameter in function javascript

--- 3_javascript.js
+++ 3_javascript.js
@@ -1,8 +1,17 @@
 // document.write("Now we using external file .js");
 
-// Function
-function saySomething(){
-    alert("This is a function");
+// // Function
+// function saySomething(){
+//     alert("This is a function");
+// }
+// // call Function
+// saySomething(); 
+
+// Function 
+function addSomething(num,string)
+{
+    var add = num + string;
+    alert(add);
 }
-// call Function
-saySomething(); 
+// call Function 
+addSomething(7," Tutorials has been finish");
