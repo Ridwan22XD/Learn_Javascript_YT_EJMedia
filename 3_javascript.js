bbcca7f9195b83de120669aef69f29ccbdf2316c
Ridwan <mre22xd@gmail.com>
2020-01-28 13:52:59 +0700

    simple condition

--- 3_javascript.js
+++ 3_javascript.js
@@ -20,41 +20,54 @@
 
 
 
-// Operator
-var number1 = 11;
-var number2 = 5;
-var number4 = 4
-var number5 = 2;
-var number6 = 3;
-var number7 = 4;
+// // Operator
+// var number1 = 11;
+// var number2 = 5;
+// var number4 = 4
+// var number5 = 2;
+// var number6 = 3;
+// var number7 = 4;
 
-document.write(
-    "*********Begin*********","<br>",
-    "Number1 = ",number1,"<br>",
-    "Number2 = ",number2,"<br>",
-    "Number4 = ",number4,"<br>",
-    "Number5 = ",number5,"<br>",
-    "Number6 = ",number6,"<br>",
-    "Number7 = ",number7,"<br>",)
+// document.write(
+//     "*********Begin*********","<br>",
+//     "Number1 = ",number1,"<br>",
+//     "Number2 = ",number2,"<br>",
+//     "Number4 = ",number4,"<br>",
+//     "Number5 = ",number5,"<br>",
+//     "Number6 = ",number6,"<br>",
+//     "Number7 = ",number7,"<br>",)
 
-// number3 = 11 + 5 = 16
-var number3 = number1 + number2;
+// // number3 = 11 + 5 = 16
+// var number3 = number1 + number2;
 
-// number1 = 11 + 5 = 16
-number1 += number2; 
+// // number1 = 11 + 5 = 16
+// number1 += number2; 
 
-// Increment
-// number2 = 5 + 1 = 6
-number2++;
+// // Increment
+// // number2 = 5 + 1 = 6
+// number2++;
 
-// Decrement 
-// number4 = 4 - 1 = 3
-number4--;
+// // Decrement 
+// // number4 = 4 - 1 = 3
+// number4--;
 
-document.write(
-    "<br>","*********Update*********","<br>",
-    "Number3 = ",number3,"<br>",
-    "Number1 = ",number1,"<br>",
-    "Number2 increment = ",number2,"<br>",
-    "Number4 decrement = ",number4,"<br>",
-    " Number5 + Number6 * Number7 = ",number5+number6*number7,"<br>");
+// document.write(
+//     "<br>","*********Update*********","<br>",
+//     "Number3 = ",number3,"<br>",
+//     "Number1 = ",number1,"<br>",
+//     "Number2 increment = ",number2,"<br>",
+//     "Number4 decrement = ",number4,"<br>",
+//     " Number5 + Number6 * Number7 = ",number5+number6*number7,"<br>");
+
+
+// Simple Condition 
+var food = "Mango"
+
+if(food == "Apple")
+{
+    alert("You have apple");
+}
+else
+{
+    alert("You didn't have apple");
+}
